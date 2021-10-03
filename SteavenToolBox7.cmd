0b9954cabb551d3149c44743d4c0de66e92fe119
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-10-03 18:08:55 +0200

    runtime now is built in

--- SteavenToolBox7.cmd
+++ SteavenToolBox7.cmd
@@ -1,6 +1,6 @@
 echo off
 mode con: cols=160 lines=78
-powershell.exe -command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SteavenGamerYT/SteavenToolBox7/main/runtime.ps1'))"
+powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 :start
 color b
 title SteavenToolBox for Windows 7! v1.0
